b5c0695b0c2e3dfc934fb2e8e49e3a140ff0040e
Jordan2204 <anafackjordan@gmail.com>
2021-09-06 14:17:48 -0400

    Final

---
+++ build/enclave/CMakeFiles/enclave.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enclave.dir/enc.c.o"
+  "CMakeFiles/enclave.dir/enc.c.o.d"
+  "CMakeFiles/enclave.dir/guessing_t.c.o"
+  "CMakeFiles/enclave.dir/guessing_t.c.o.d"
+  "enclave"
+  "enclave.pdb"
+  "guessing_args.h"
+  "guessing_t.c"
+  "guessing_t.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/enclave.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/host/CMakeFiles/guessing_host.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guessing_host.dir/guessing_u.c.o"
+  "CMakeFiles/guessing_host.dir/guessing_u.c.o.d"
+  "CMakeFiles/guessing_host.dir/host.c.o"
+  "CMakeFiles/guessing_host.dir/host.c.o.d"
+  "guessing_args.h"
+  "guessing_host"
+  "guessing_host.pdb"
+  "guessing_u.c"
+  "guessing_u.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/guessing_host.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
